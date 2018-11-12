8ca96c3ce2ff18014a9a536bd86373b89fd63150
Albert Abril <albert.abril@gmail.com>
2018-11-12 19:40:35 +0000

    get rid of routes, just a preference to name it as controllers (or handlers), and then routes will be http definitions

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const index = require('./routes/index.js');
+require('express-jsend'); // https://www.npmjs.com/package/express-jsend
 
-app.get('/', index);
 
-app.listen(3000, () => console.log(`Open http://localhost:3000 to see a response.`));
+const indexController = require('./controllers/index.controller.js');
+
+// routes
+function setRoutes(app){
+  app.get('/', indexController);
+}
+
+setRoutes(app)
+
+app.listen(PORT, () => console.log(`Open http://localhost:3000 to see a response.`));
+
+module.exports = app
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * The default index route handler.
- * Responds to a request with body content to demonstrate the app is running as expected.
- */
-module.exports = (req, res, next) => {
-  res.end(`Express Code Challenge Started`);
-}

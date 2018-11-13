9d43ab2a712b81b6a797ac73306d814ec436292e
Albert Abril <albert.abril@gmail.com>
2018-11-13 16:31:33 +0000

    update book route param to find by isbn (not decided yet, in progress)

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,13 @@ const institutionController = require('./controllers/institution.controller.js')
 // routes
 function setRoutes(app){
   app.get('/', indexController);
-
+  
   app.get('/api/book', bookController.list);
-  app.get('/api/book/:id', bookController.item);
+  app.get('/api/book/:isbn', bookController.item);
   app.post('/api/book', bookController.create);
   app.put('/api/book/:id', bookController.update);
   app.patch('/api/book/:id', bookController.update);
   app.delete('/api/book/:id', bookController.destroy);
-
 }
 
 module.exports = setRoutes
